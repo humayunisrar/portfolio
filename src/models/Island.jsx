92e8c53ab12971f0710dda09d2b039a1abcaf4ec
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2023-12-22 20:04:28 +0500

    rotation endingg

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -53,15 +53,70 @@ const Island = ({isRotating, setIsRotating, ...props}) => {
     }
   }
 
+  const handleKeyDown =(e) =>{
+    if(e.key ==='ArrowLeft'){
+      if(!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y += 0.01*Math.PI;
+    } else if(e.key ==='ArrowRight'){
+      if(!isRotating) setIsRotating(true);
+      islandRef.current.rotation.y -= 0.01*Math.PI;
+
+    }
+  }
+
+  const handleKeyUp =(e) =>{
+    if(e.key  ==='ArrowLeft' || e.key === 'ArrowRight'){
+      setIsRotating(false);
+    }
+  }
+
+  useFrame(() =>{
+    if(!isRotating){
+      rotationSpeed.current *=dampingFactor;
+
+      if(Math.abs(rotationSpeed.current) < 0.001){
+        rotationSpeed.current=0;
+      }
+    }else{
+      const rotation = islandRef.current.rotation.y;
+    }
+    const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+  })
+
   useEffect(() => {
-    document.addEventListener('pointerdown',handlePointerDown);
-    document.addEventListener('pointerup',handlePointerUp);
-    document.addEventListener('pointermove',handlePointerMove);
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown',handlePointerDown);
+    canvas.addEventListener('pointerup',handlePointerUp);
+    canvas.addEventListener('pointermove',handlePointerMove);
+    document.addEventListener('keydown',handleKeyDown);
+    document.addEventListener('keyup',handleKeyUp);
+
     
     return () => {
-      document.removeEventListener('pointerdown',handlePointerDown);
-      document.removeEventListener('pointerup',handlePointerUp);
-      document.removeEventListener('pointermove',handlePointerMove);
+      canvas.removeEventListener('pointerdown',handlePointerDown);
+      canvas.removeEventListener('pointerup',handlePointerUp);
+      canvas.removeEventListener('pointermove',handlePointerMove);
+      document.removeEventListener('keydown',handleKeyDown);
+      document.removeEventListener('keyup',handleKeyUp);
     }
 
   },[gl, handlePointerDown,handlePointerUp, handlePointerMove])
