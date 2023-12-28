57b07d509f3df2ffb4cbce9cb3b1934e3fcd2eb8
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2023-12-28 18:52:36 +0500

    Update Bird.jsx

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -15,19 +15,25 @@ const Bird = () => {
 
   }, []);
   
-
-useFrame(({clock, camera}) =>{
-  birdRef.current.rotation.y += Math.sin (clock.elapsedTime)* 0.2 +2;
-  if(birdRef.current.rotation.y ===0){
-
-    birdRef.current.rotation.x += 0.01;
-    birdRef.current.rotation.z -= 0.01;
-  }else{
-    birdRef.current.rotation.x -= 0.01;
-    birdRef.current.rotation.z += 0.01;
+  useFrame(({ clock, camera }) => {
+   
+     birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
   
-  }
-})
+      if (birdRef.current.position.x > camera.position.x + 10) {
+     
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      
+      birdRef.current.rotation.y = 0;
+    }
+    if (birdRef.current.rotation.y === 0) {
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
+    }
+  });
 
   return (
    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
