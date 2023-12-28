eb66eeeac83ba028ad07485ef24c451dad43c662
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2023-12-28 21:39:00 +0500

    update popups

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const InfoBox =({text, link, btnText}) => {
+    
+    <div className='info-box'>
+        {text}
+        <Link to={link}>
+            {btnText}
+        </Link>
+    </div>
+}
 
 const renderContent={
     1: (
-        <h1>1</h1>
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue px-8 py-8 text-white mx-5'>
+            Hi, I'm <span className='font-semibold'>Humayun </span>👋
+        <br/>
+          A Website Developer From Pakistan
+        </h1>
     ),
     2: (
-        <h1>2</h1>
+        
+        <InfoBox
+        text={"Here's some of my work."}
+        link={"/about"}
+        btnText={"See More!"}
+        />
     ),
     3: (
         <h1>3</h1>
@@ -15,6 +35,7 @@ const renderContent={
     ),
 
 }
+
 
 const HomeInfo = ({currentStage}) => {
   return renderContent [currentStage] || null;
