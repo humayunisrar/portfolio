0702d0b4db1cec7b4215413889b6a1541ca88a9c
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2023-12-30 15:44:21 +0500

    Fox.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,8 @@
-import {useRef, useState} from 'react'
+import {Suspense, useRef, useState} from 'react'
 import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+import { Fox } from '../models/Fox';
+import { Loader } from '../components/Loader';
 
 const Contact = () => {
 
@@ -22,9 +25,9 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
-        to_name: 'Humayun',
+        to_name: "Humayun",
         from_email: form.email,
-        to_email: 'humayunisrar31@gmail.com',
+        to_email: "humayunisrar31@gmail.com",
         message: form.message,
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
@@ -37,7 +40,7 @@ const Contact = () => {
       //TODO Hide an alert
     }).catch(()=>{
       setIsLoading(false);
-      console.log(error);
+      // console.log(error);
     }) 
   };
 
@@ -46,9 +49,9 @@ const Contact = () => {
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
 
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form className='w-full flex flex-col gap-7 mt-14'  onSubmit={handleSubmit}>
           
-          onSubmit={handleSubmit}
+         
 
           <label className='text-black-500 font-semibold'>Name</label>
           <input 
@@ -100,6 +103,14 @@ const Contact = () => {
               {isLoading ? 'Sending...' : 'Send Message'}</button>
 
         </form>
+      </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+      <Canvas>
+        camera={{ position: [0, 0, 5] }}
+        <Suspense fallback={<Loader/>}>
+          <Fox/>
+          </Suspense>
+      </Canvas>
       </div>
     </section>
   )
