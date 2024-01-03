6847e8b8346c95bfe541bfd904cc575c15bf8ef1
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-01-03 17:23:47 +0500

    About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,7 @@
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import React from 'react'
-import { skills } from '../constants'
+import { skills, experiences } from '../constants'
 
 
 const About = () => {
@@ -33,12 +35,52 @@ const About = () => {
   <h3 className='subhead-text'>Work Experience</h3>
   <div className='mt-5 flex flex-col gap-3 text-slate-500'>
       <p>Here's  a run down to my previous experience:  </p>
+
+      <div className='mt-12 flex'>
+
+          <VerticalTimeline>
+            {experiences.map(experience => (
+              <VerticalTimelineElement key={experience.company_name } date={experience.date} icon={<div className='flex justify-center items-center w-full h-full'>
+                <img src={experience.icon}
+                alt={experience.company_name}
+                className='w-[60%] h-[60%] object-contain'
+                />
+              </div>} 
+              iconStyle={{
+                background: experience.iconBg,
+                boxShadow: 'none',
+              
+              }}
+              contentStyle={{
+                borderBottom:'8px',
+                borderStyle:'solid',
+                borderBottomColor: experience.iconBg,
+                boxShadow: 'none',
+              }}>
+
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p className='text-black-500 font-medium font-base' style={{margin:0}}>
+                    {experience.company_name}
+                  </p>
+                </div>
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point,index) => (
+                    <li key={`experience-point-${index}`} className='text-black-500/50 font-normal pl-1 text-sm'>
+                      {point}
+                    </li>
+
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+      </div>
     </div>
 </div>
-
    </section>
-
-  )
-}
+  )}
 
 export default About
