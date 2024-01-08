7393c45e8af2c4925447ab50647fa8b224a57334
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-01-08 20:42:54 +0500

    edit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <header className='header'>
-        <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
-            <p className="blue-gradient_text">HI</p>
-
+        <NavLink to="/" className="w-12 h-11 rounded-lg bg-white items-center justify-center flex shadow-md p-1">
+            <img src="/logo.svg" alt="logo" />
         </NavLink>
+
         <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
           About
