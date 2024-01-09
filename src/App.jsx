05c6a922accd3fbd85f665265529f6f9f4da3104
Humayun Israr <94983653+humayunisrar@users.noreply.github.com>
2024-01-09 13:05:51 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,28 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-   <main className="bg-slate-300/20 h-full">
-<Router>
-  <Navbar />
-  <Routes>
-    <Route path='/' element={<Home/>}/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/projects' element={<Projects/>}/>
-    <Route path='/contact' element={<Contact/>}/>
-  </Routes>
-  <Footer/>
-</Router>
+    <main className='bg-slate-300/20'>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route
+          path='/*'
+          element={
+            <>
+              <Routes>
+                <Route path='/about' element={<About />} />
+                <Route path='/projects' element={<Projects />} />
+                <Route path='/contact' element={<Contact />} />
+              </Routes>
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
+  </main>
 
-   </main>
   )
 }
 
